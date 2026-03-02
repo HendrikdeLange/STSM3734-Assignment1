ce34c25b67bddc4faadafd7bc59b492b7c20c9dd
Hendrik <dlcapital.dev@gmail.com>
2026-03-02 13:43:28 +0200

    Added observational data

---
+++ obs_data_analysis.R
@@ -0,0 +1,2 @@
+df <- read.csv("~/STSM3734-MAIZE_SIMULATION/obs_data/Observational_Group14.csv")
+
---
+++ test_hendrik.R
@@ -0,0 +1,4 @@
+library(GGally)
+df <- read.csv("C:\\Users\\hendr\\OneDrive\\Desktop\\maize_obervations_export.csv")
+
+ggpairs(df)
